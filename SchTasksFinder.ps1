3ba8469ec5b0ca16996f7fb27d79b68991daaabb
DcardosoGH <diogo.g.c@gmail.com>
2023-12-18 12:51:39 +0000

    +++

--- SchTasksFinder.ps1
+++ SchTasksFinder.ps1
@@ -1,23 +1,37 @@
 function FindCLSIDInRegistry($clsid) {
+    $hkcrPath = if (Test-Path "HKCR:\") {"HKCR:\"} else {"HKLM:\SOFTWARE\Classes\"}
     $paths = @(
-        "HKCR:\CLSID\",
+        $hkcrPath,
         "HKLM:\SOFTWARE\Classes\CLSID\"
     )
 
     foreach ($path in $paths) {
         $regPath = Join-Path -Path $path -ChildPath $clsid
-        if (Test-Path $regPath) {
-            return $regPath
+        try {
+            if (Test-Path $regPath -ErrorAction SilentlyContinue) {
+                return $regPath
+            }
+        } catch {
+            # Error is caught but not displayed
         }
     }
 
     return $null
 }
 
-$Tasks = Get-ScheduledTask
+try {
+    $Tasks = Get-ScheduledTask -ErrorAction SilentlyContinue
+    if ($null -eq $Tasks) {
+        Write-Host "No scheduled tasks found or access denied."
+        return
+    }
+} catch {
+    Write-Host "Error retrieving scheduled tasks."
+    return
+}
 
 foreach ($Task in $Tasks) {
-    if ($Task.Actions.ClassId -ne $null -and $Task.Triggers.Enabled -eq $true -and $Task.Principal.GroupId -eq "Users") {
+    if ($Task -and $Task.Actions.ClassId -ne $null -and $Task.Triggers.Enabled -eq $true -and $Task.Principal.GroupId -eq "Users") {
         Write-Host "Task Name: " $Task.TaskName
         Write-Host "Task Path: " $Task.TaskPath
         Write-Host "CLSID: " $Task.Actions.ClassId
